12bb012259d99a0695e5c46cdc8589ee773efbc0
KSHATest <sklyzet@yandex.ru>
2024-03-21 20:02:07 +0500

    Initial commit

---
+++ src/test/java/ru/netology/javaqa/HW4/services/FreelancerServiceTest.java
@@ -0,0 +1,22 @@
+package ru.netology.javaqa.HW4.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FreelancerServiceTest {
+
+    @Test
+    public void test1() {
+        FreelancerService freelancerService = new FreelancerService();
+        int result = freelancerService.calculate(10000, 3000, 20000);
+        assertEquals(3, result);
+    }
+
+    @Test
+    public void test2() {
+        FreelancerService freelancerService = new FreelancerService();
+        int result = freelancerService.calculate(100000, 60000, 150000);
+        assertEquals(2, result);
+    }
+}
